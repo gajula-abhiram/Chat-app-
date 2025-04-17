0a4c0a80c50e6b7c14594df2d1f6d69cdbe626e4
gajula-abhiram <abhiramgajula002@gmail.com>
2025-04-18 02:49:03 +0530

    Initial commit for Chat App

---
+++ frontend/src/components/features/navbar/Logo.jsx
@@ -0,0 +1,16 @@
+const Logo = () => {
+  // changed logo
+  return (
+    <div className='hidden md:flex flex-1 cursor-pointer'>
+      <img
+        src='/avatar-logo.svg'
+        alt='logo chat'
+        width='44px'
+        height='44px'
+        className='w-[44px] h-[44px]'
+      />
+    </div>
+  );
+};
+
+export default Logo;
---
+++ frontend/src/shared/hooks/useDeleteGroup.jsx
@@ -0,0 +1,34 @@
+import { useCallback, useState } from 'react';
+import toast from 'react-hot-toast';
+
+export const useDeleteGroup = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  // for loading screen
+
+  const deleteGroup = useCallback(async ({ users, groupId }) => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(`/api/chat/group/${groupId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ users }),
+      });
+
+      const data = await res.json();
+
+      if (data.error || data.message) {
+        throw new Error(data.error || data.message);
+      }
+
+      toast.success(data.info);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  return { isLoading, deleteGroup };
+};
